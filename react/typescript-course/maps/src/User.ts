7146aa995b366fbd8ee758f90b36031495fc3b1f
Yingying Chen <dinnerparty718@gmail.com>
2023-02-02 11:25:33 -0700

    added hey

---
+++ react/typescript-course/maps/src/User.ts
@@ -0,0 +1,22 @@
+import { faker } from "@faker-js/faker";
+import { Mappable } from './Map';
+
+export class User implements Mappable {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  }
+  color: string;
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = { lat: parseFloat(faker.address.latitude()), lng: parseFloat(faker.address.longitude()) }
+    this.color = 'red';
+  }
+
+
+  markerContent() {
+    return `User Name: ${this.name}`;
+  }
+}
